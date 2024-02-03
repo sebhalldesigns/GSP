b26fc8a3d28bee1a8239f67bae5160863ac00cdc
sebhall <sebhalldesigns@gmail.com>
2024-02-03 17:51:34 +0000

    initial compilation

---
+++ src/Graphics/include/GspGraphics.h
@@ -0,0 +1,26 @@
+//  GspGraphics.h - System - GSP
+//  First created by Seb Hall on 03/Feb/2024
+//
+//  GSP is a cross-platform software framework
+//  that provides abstraction layers, libraries,
+//  packaging and build functionality. GSP is
+//  designed around a modular architecture that
+//  promotes simplicity and encapsulation, and
+//  we hope that you will do amazing things with
+//  it!
+//
+//  GspGraphics.h is an abstraction layer over
+//  the raw platform APIs such as Vulkan, Metal
+//  and OpenGL.
+
+#ifndef GSPGRAPHICS_H
+#define GSPGRAPHICS_H
+
+enum gGraphicsApi {
+    OPENGL,
+    VULKAN,
+    METAL
+};
+
+
+#endif // GSPGRAPHICS_H
