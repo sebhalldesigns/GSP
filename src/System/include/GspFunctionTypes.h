528d283ae1bdd1d1ef6b52be6188d81179682c67
sebhall <sebhalldesigns@gmail.com>
2024-02-03 12:49:43 +0000

    some starter files

---
+++ src/System/include/GspFunctionTypes.h
@@ -0,0 +1,28 @@
+//  GspFunctionTypes.h - System - GSP
+//  First created by Seb Hall on 03/Feb/2024
+//
+//  GSP is a cross-platform software framework
+//  that provides abstraction layers, libraries,
+//  packaging and build functionality. GSP is
+//  designed around a modular architecture that
+//  promotes simplicity and encapsulation, and
+//  we hope that you will do amazing things with
+//  it!
+//
+//  GspFunctionTypes.h contains types for GSP
+//  interfaces.
+
+#ifndef GSPFUNCTIONTYPES_H
+#define GSPFUNCTIONTYPES_H
+
+enum gResult {
+    SUCCESS,
+    WARNING,
+    ERROR,
+    FAILURE
+};
+
+
+
+
+#endif // GSPFUNCTIONTYPES_H
